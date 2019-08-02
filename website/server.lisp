f7b66fbc8c007b7ec07eed9208931ffda97585af
xh <xh@516c18b2-57ce-4e5b-bd1a-144cc7c44f57>
2019-08-02 09:36:26 +0000

    git-svn-id: svn://svn.coobii.com/source/wt@549 516c18b2-57ce-4e5b-bd1a-144cc7c44f57

--- website/server.lisp
+++ website/server.lisp
@@ -6,15 +6,22 @@
   (declare (ignore handler request))
   (setf (response-status *response*) 200)
   (setf (header-field *response* "Content-Type") "text/html")
-  (setf (response-body *response*) "Lisp Web Toolkit"))
+  (setf (response-body *response*)
+        (html:serialize
+         (html:document
+          (html:html
+           (html:head
+            (html:title (html:text "Lisp Web Toolkit")))
+           (html:body
+            (html:h1 (html:text "Lisp Web Toolkit"))))))))
 
 (define-server website-server
     :handler website-handler
     :listeners (list
-                (listener :port 8004)))
+                (listener :port 8080)))
 
 (defun start-server ()
   (http:start-server website-server))
 
 (defun stop-server ()
-  (http:stop-server website-server))
+    (http:stop-server website-server))
