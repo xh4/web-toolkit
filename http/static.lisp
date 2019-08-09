061224deef11a25ae5f01e91d8d6b04416e0ac98
xh <xh@516c18b2-57ce-4e5b-bd1a-144cc7c44f57>
2019-08-09 06:12:39 +0000

    git-svn-id: svn://svn.coobii.com/source/wt@563 516c18b2-57ce-4e5b-bd1a-144cc7c44f57

--- http/static.lisp
+++ http/static.lisp
@@ -14,3 +14,64 @@
          (relative-path (path-trim-prefix prefix (request-uri request)))
          (pathname (merge-pathnames relative-path location)))
     (setf (response-body *response*) pathname)))
+
+(defmethod build-routing-rule ((type (eql :static)) form)
+  (apply #'build-static-routing-rule (rest form)))
+
+;; TODO: Check path normativity
+(defun path-segments (path)
+  (split-sequence #\/ (subseq path 1)))
+
+(defun path-trim-prefix (prefix path)
+  (if (or (null prefix)
+          (equal prefix "")
+          (equal prefix "/"))
+      (subseq path 1)
+      (let ((path-segments (path-segments path))
+            (prefix-segments (split-sequence #\/ (string-trim "/" prefix)))
+            (suffix-segments '())
+            (match-prefix-p nil))
+        (loop for end from 1 upto (length path-segments)
+           for head-segments = (subseq path-segments 0 end)
+           for tail-segments = (subseq path-segments end)
+           when (equal head-segments prefix-segments)
+           do (setf suffix-segments tail-segments
+                    match-prefix-p t))
+        (when match-prefix-p
+          (if (null suffix-segments)
+              ""
+              (format nil "~{~A~^/~}" suffix-segments))))))
+
+;; (path-trim-prefix "foo" "/foo")
+;; (path-trim-prefix "foo" "/foo/")
+;; (path-trim-prefix "foo" "/foo/bar")
+;; (path-trim-prefix "/foo" "/foo/bar")
+;; (path-trim-prefix "foo/" "/foo/bar")
+;; (path-trim-prefix "/foo/" "/foo/bar")
+;; (path-trim-prefix "/foo/bar" "/foo/bar")
+;; (path-trim-prefix "/foo/bar" "/foo/bar/")
+;; (path-trim-prefix "/foo/bar/" "/foo/bar")
+;; (path-trim-prefix "/foo/bar/" "/foo/bar/")
+;; (path-trim-prefix "goo" "/foo/bar/")
+;; (path-trim-prefix "/" "/foo/bar/")
+;; (path-trim-prefix "" "/foo/bar")
+;; (path-trim-prefix nil "/foo/bar/")
+
+(defun build-static-routing-rule (&key prefix location)
+  (check-type location (or string pathname))
+  (when (stringp location)
+    (setf location (pathname location)))
+  (unless (uiop:absolute-pathname-p location)
+    (error "Location must be absolute"))
+  (setf location (uiop:ensure-directory-pathname location))
+
+  (let ((rule (make-instance 'routing-rule)))
+    (let ((matcher (lambda (request)
+                     (and (eq (request-method request) :get)
+                          (path-trim-prefix prefix (request-uri request))))))
+      (setf (routing-rule-matcher rule) matcher))
+    (let ((handler (make-instance 'static-handler
+                                  :prefix prefix
+                                  :location location)))
+      (setf (routing-rule-handler rule) handler))
+    rule))
