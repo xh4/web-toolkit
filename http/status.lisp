0a6f8ff9b7d6a6266d09cc24948cdb42002ccde4
xh <xh@516c18b2-57ce-4e5b-bd1a-144cc7c44f57>
2019-08-11 11:05:35 +0000

    git-svn-id: svn://svn.coobii.com/source/wt@570 516c18b2-57ce-4e5b-bd1a-144cc7c44f57

--- http/status.lisp
+++ http/status.lisp
@@ -1,23 +1,17 @@
 (in-package :http)
 
-(defclass status ()
-  ((keyword
-    :initarg :keyword
-    :reader status-keyword)
-   (code
-    :initarg :code
-    :reader status-code)
-   (reason-phrase
-    :initarg :reason-phrase
-    :reader status-reason-phrase)))
-
-(defmethod print-object ((status status) stream)
-  (print-unreadable-object (status stream)
-    (format stream "~A ~A"
-            (status-code status)
-            (status-reason-phrase status))))
-
 (eval-when (:compile-toplevel :execute :load-toplevel)
+
+  (defclass status ()
+    ((keyword
+      :initarg :keyword
+      :reader status-keyword)
+     (code
+      :initarg :code
+      :reader status-code)
+     (reason-phrase
+      :initarg :reason-phrase
+      :reader status-reason-phrase)))
 
   (defvar *statuses* '())
 
@@ -34,6 +28,12 @@
                      (nconc *statuses*
                             (list status)))
                status))))))
+
+(defmethod print-object ((status status) stream)
+  (print-unreadable-object (status stream)
+    (format stream "~A ~A"
+            (status-code status)
+            (status-reason-phrase status))))
 
 ;; https://tools.ietf.org/html/rfc7231#section-6.1
 
