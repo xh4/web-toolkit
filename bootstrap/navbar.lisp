61c8960c6bf1dc3ada76149943616531475117cd
Xiangyu He <xh@coobii.com>
2019-08-15 17:28:08 +0800

    Update navbar.lisp

--- bootstrap/navbar.lisp
+++ bootstrap/navbar.lisp
@@ -1,14 +1,24 @@
 (in-package :bootstrap)
 
 (define-component navbar ()
-  ())
+  ()
+  (:tag-option tag :initform :nav)
+  (:class-option class :default "navbar"))
 
-(define-render navbar ()
-  )
-
+(define-render navbar (tag class children)
+  (tag :class class
+       @children))
 
 (define-component navbar-brand ()
-  ())
+  ((link
+    :initarg :link
+    :initform nil))
+  (:tag-option tag :initform :span)
+  (:class-option class :default "navbar-brand"))
 
-(define-render navbar-brand ()
-  )
+(define-render navbar-brand (tag class children link)
+  (when link
+    (setf tag :a))
+  (tag :class class
+       :href link
+       @children))
