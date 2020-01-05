c9b8a4a3f7364b31de6a2e541057ff50c719faad
Xiangyu He <xh@coobii.com>
2020-01-05 17:58:35 +0800

    Fix URI decode tests

--- test/uri/decode.lisp
+++ test/uri/decode.lisp
@@ -3,6 +3,6 @@
 (in-suite :uri-test)
 
 (test percent-decode
-  (is (equal (uri::percent-decode "%63") "c"))
-  (is (equal (uri::percent-decode "ab%63") "abc"))
-  (is (equal (uri::percent-decode "%E2%9D%A4") "❤")))
+  (is (equal (uri::percent-decode-string "%63") "c"))
+  (is (equal (uri::percent-decode-string "ab%63") "abc"))
+  (is (equal (uri::percent-decode-string "%E2%9D%A4") "❤")))
