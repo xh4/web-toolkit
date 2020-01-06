09aeb39dc776cc48f1e76658eff7855311754c7b
Xiangyu He <xh@coobii.com>
2020-01-06 14:01:37 +0800

    Add more tests

--- test/uri/decode.lisp
+++ test/uri/decode.lisp
@@ -6,4 +6,5 @@
   (is (equal (uri::percent-decode "%63") "c"))
   (is (equal (uri::percent-decode "ab%63") "abc"))
   (is (equal (uri::percent-decode "%E2%9D%A4") "❤"))
-  (is (equal (uri::percent-decode "%E7%88%B1") "爱")))
+  (is (equal (uri::percent-decode "%E7%88%B1") "爱"))
+  (is (equal (uri::percent-decode "爱") "爱")))
--- test/uri/encode.lisp
+++ test/uri/encode.lisp
@@ -4,4 +4,8 @@
 
 (test percent-encode
   (is (equal (uri::percent-encode "❤") "%E2%9D%A4"))
-  (is (equal (uri::percent-encode "爱") "%E7%88%B1")))
+  (is (equal (uri::percent-encode "爱") "%E7%88%B1"))
+  (is (equal (uri::percent-encode "abc") "%61%62%63"))
+  (is (equal (uri::percent-encode "abc" :reserve 'uri::alpha-p) "abc"))
+  (is (equal (uri::percent-encode "abc123" :reserve 'uri::alpha-p)
+             "abc%31%32%33")))
