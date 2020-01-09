6be74aa877189a31bd636099bf218958732cd270
Xiangyu He <xh@coobii.com>
2020-01-09 13:55:10 +0800

    Add merge-uri abnormal examples

--- test/uri/merge.lisp
+++ test/uri/merge.lisp
@@ -6,15 +6,15 @@
   (is (equal "/a/g" (uri::remove-dot-segments "/a/b/c/./../../g")))
   (is (equal "mid/6" (uri::remove-dot-segments "mid/content=5/../6"))))
 
-(defmacro test-merge-uri (&body forms)
+(defmacro test-merge-uri (test-name &body forms)
   (let ((base-uri "http://a/b/c/d;p?q"))
-    `(test merge-uri
+    `(test ,test-name
        ,@(loop for (target-uri relative-uri) in forms
             collect `(is (equal ,target-uri
                                 (uri-string
-                                 (merge-uri ,base-uri ,relative-uri))))))))
+                                 (uri:merge-uri ,base-uri ,relative-uri))))))))
 
-(test-merge-uri
+(test-merge-uri merger-uri-normal
  ("g:h" "g:h")
  ("http://a/b/c/g" "g")
  ("http://a/b/c/g" "./g")
@@ -38,3 +38,23 @@
  ("http://a/" "../..")
  ("http://a/" "../../")
  ("http://a/g" "../../g"))
+
+(test-merge-uri merge-uri-abnormal
+  ("http://a/g" "../../../g")
+  ("http://a/g" "../../../../g")
+  ("http://a/g" "/./g")
+  ("http://a/g" "/../g")
+  ("http://a/b/c/g." "g.")
+  ("http://a/b/c/.g" ".g")
+  ("http://a/b/c/g.." "g..")
+  ("http://a/b/c/..g" "..g")
+  ("http://a/b/g" "./../g")
+  ("http://a/b/c/g/" "./g/.")
+  ("http://a/b/c/g/h" "g/./h")
+  ("http://a/b/c/h" "g/../h")
+  ("http://a/b/c/g;x=1/y" "g;x=1/./y")
+  ("http://a/b/c/y" "g;x=1/../y")
+  ("http://a/b/c/g?y/./x" "g?y/./x")
+  ("http://a/b/c/g?y/../x" "g?y/../x")
+  ("http://a/b/c/g#s/./x" "g#s/./x")
+  ("http://a/b/c/g#s/../x" "g#s/../x"))
