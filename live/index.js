4e2720e68f5fb46169051a177b90b9401c436025
Xiangyu He <xh@coobii.com>
2020-01-15 08:28:03 +0800

    Remove wt.live

--- live/index.js
+++
@@ -1,7 +0,0 @@
-wt.live.connect('ws://118.190.145.4:8003/live')
-    .then(function() {
-        setTimeout(function() {
-            wt.live.call({type: "symbol",
-                  name: "+"}, 1, 2, 3, 4);
-        }, 1000);
-    });
