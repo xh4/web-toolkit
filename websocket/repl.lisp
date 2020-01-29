7e063e21020565907e53e411ec113c240938c2e8
Xiangyu He <xh@coobii.com>
2020-01-30 01:31:06 +0800

    Add repl.lisp

---
+++ websocket/repl.lisp
@@ -0,0 +1,23 @@
+(in-package :websocket)
+
+;; TODO: 处理 session 关闭的情况
+;; TODO: 处理抛出异常的情况
+(defun in-session (session)
+  (when *session*
+    (error "Already in session ~A" *session*))
+  (unless session
+    )
+  (unless (typep session 'session)
+    )
+  (loop
+     (let ((*session* session))
+       (format t "~A : ~A > " (package-name *package*) session)
+       (finish-output)
+       (let ((input-form (read *standard-input* nil nil)))
+         (let ((result (case input-form
+                         (:q (return session))
+                         (t (handler-case
+                                (eval input-form)
+                              (error (e)
+                                (invoke-debugger e)))))))
+           (format t "~A~%" result))))))
