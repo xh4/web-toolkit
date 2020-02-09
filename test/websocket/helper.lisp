e1feb5e1a0da990dee24356cba02f81f01323525
Xiangyu He <xh@coobii.com>
2020-02-09 17:11:24 +0800

    Fix find-executable on linux

--- test/websocket/helper.lisp
+++ test/websocket/helper.lisp
@@ -2,7 +2,8 @@
 
 (defun find-executable (name &rest optional-names)
   (when-let ((path (uiop:getenv "PATH")))
-    (let ((paths (split-sequence #\; path)))
+    (let ((paths #+win32 (split-sequence #\; path)
+                 #+unix (split-sequence #\: path)))
       (loop for path in paths
          for exe-path = (loop for name in (append (list name) optional-names)
                            for exe-path = (merge-pathnames
