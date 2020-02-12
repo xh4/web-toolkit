79469e113c1297b49e780a30c236fdb897d86d40
Xiangyu He <xh@coobii.com>
2020-02-12 19:26:20 +0800

    Test redirect

--- test/http/redirect.lisp
+++ test/http/redirect.lisp
@@ -3,4 +3,17 @@
 (in-suite :http-test)
 
 (test redirect
-  )
+  (it "should redirect"
+      (define-handler foo ()
+        ()
+        (:function (lambda ()
+                     (redirect "/foo")
+                     (reply (status 200)))))
+
+      (let ((res (http::invoke-handler foo nil)))
+        (is (typep res 'response))
+        (is (equal 307 (status-code (response-status res))))
+        (is (equal "/foo" (header-field-value
+                           (find-header-field
+                            "Location"
+                            res)))))))
