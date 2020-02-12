3eaa4d4222c9ea55249a9897759f7a6e072314da
Xiangyu He <xh@coobii.com>
2020-02-12 17:35:53 +0800

    Fix test report

--- test/fiveam.lisp
+++ test/fiveam.lisp
@@ -29,29 +29,26 @@
 
 (in-package :fiveam)
 (defun process-failure (test-expr &optional reason-format &rest format-args)
-  (let ((reason (and reason-format
-                     (apply #'format nil reason-format format-args)))
-        (premble))
-    (setf test:*it*
-          (test::map-tree
-           (lambda (form)
-             (if (and (listp form)
-                      (not (emptyp form))
-                      (equal (first form) 'test:=>))
-                 (throw :skip
-                   (if (equal (second test:*is*) (second form))
-                       (cons 'test:=> (cddr form))
-                       (cddr form)))
-                 form))
-           test:*it*
-           :tag :skip))
-    (loop for form in test:*it*
-       do (setf premble (concatenate 'string
-                                     premble
-                                     (format nil "~%IN ~%~A~%"
-                                             (cl:with-output-to-string (stream)
-                                               (pprint form stream))))))
-    (setf reason (concatenate 'string premble reason))
+  (setf test:*it*
+        (test::map-tree
+         (lambda (form)
+           (if (and (listp form)
+                    (not (emptyp form))
+                    (equal (first form) 'test:=>))
+               (throw :skip
+                 (if (equal (second test:*is*) (second form))
+                     (cons 'test:=> (cddr form))
+                     (cddr form)))
+               form))
+         test:*it*
+         :tag :skip))
+  (let ((reason (with-output-to-string (stream)
+                  (format stream "~%IN")
+                  (loop for form in test:*it*
+                     do (format stream "~%")
+                       (pprint form stream))
+                  (when reason-format
+                    (apply 'format stream reason-format format-args)))))
     (with-simple-restart (ignore-failure "Continue the test run.")
       (error 'check-failure :test-expr test-expr
              :reason reason))
