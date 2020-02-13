dc56792349cdb6c89544f2efaca9f1ae45908f46
Xiangyu He <xh@coobii.com>
2020-02-13 10:21:51 +0800

    Fix package lock

--- http/directory.lisp
+++ http/directory.lisp
@@ -4,7 +4,7 @@
   ((pathname
     :initarg :pathname
     :initform nil
-    :accessor pathname)
+    :accessor directory-pathname)
    (index
     :initarg :index
     :initform nil
@@ -13,3 +13,8 @@
 (defmethod print-object ((directory directory) stream)
   (print-unreadable-object (directory stream :type t :identity t)
     (format stream "~A" (pathname directory))))
+
+(in-package :cl-user)
+(defmethod pathname ((directory http::directory))
+  (http::directory-pathname directory))
+(in-package :http)
--- http/file.lisp
+++ http/file.lisp
@@ -4,8 +4,13 @@
   ((pathname
     :initarg :pathname
     :initform nil
-    :accessor pathname)))
+    :accessor file-pathname)))
 
 (defmethod print-object ((file file) stream)
   (print-unreadable-object (file stream :type t :identity t)
     (format stream "~A" (pathname file))))
+
+(in-package :cl-user)
+(defmethod pathname ((file http::file))
+  (http::file-pathname file))
+(in-package :http)
