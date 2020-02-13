8f3369fb077aeecf459bd48bec426ecce98f70e6
Xiangyu He <xh@coobii.com>
2020-02-14 02:24:39 +0800

    Fix file test

--- test/http/file.lisp
+++ test/http/file.lisp
@@ -7,7 +7,8 @@
     (with-static-files (root "abc")
       (with-open-file (stream (merge-pathnames "abc" root)
                               :direction :output
-                              :element-type '(unsigned-byte 8))
+                              :element-type '(unsigned-byte 8)
+                              :if-exists :supersede)
         (write-sequence (make-array 42 :initial-element 42) stream))
       (let ((file (make-instance 'http::file
                                  :pathname (merge-pathnames "abc" root))))
