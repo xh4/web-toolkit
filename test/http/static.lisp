cbc5f54ad311f024fc0bd1a86f9f2b8f42a9e2f0
Xiangyu He <xh@coobii.com>
2020-02-13 12:16:25 +0800

    Add static handler tests

--- test/http/static.lisp
+++ test/http/static.lisp
@@ -17,3 +17,86 @@
   (is (equal "foo/bar/" (http::path-trim-prefix "/" "/foo/bar/")))
   (is (equal "foo/bar" (http::path-trim-prefix "" "/foo/bar")))
   (is (equal "foo/bar/" (http::path-trim-prefix nil "/foo/bar/"))))
+
+(test path-trim-prefix/with-dots
+  (is (equal "" (http::path-trim-prefix "" "/..")))
+  (is (equal "foo" (http::path-trim-prefix "" "/../foo")))
+  (is (equal "foo" (http::path-trim-prefix "" "/.././foo")))
+  (is (equal "foo" (http::path-trim-prefix "" "/../../foo")))
+  (is (equal nil (http::path-trim-prefix "foo" "/../foo/../bar"))))
+
+(test path-prefix-p
+  (is-true (http::path-prefix-p "" "/"))
+  (is-true (http::path-prefix-p "/" "/"))
+  (is-true (http::path-prefix-p "foo" "/foo"))
+  (is-true (http::path-prefix-p "foo/" "/foo"))
+  (is-true (http::path-prefix-p "/foo" "/foo"))
+  (is-true (http::path-prefix-p "/foo/" "/foo"))
+  (is-true (http::path-prefix-p "/foo" "/foo/"))
+  (is-true (http::path-prefix-p "/foo/" "/foo/"))
+  (is-true (http::path-prefix-p "/foo" "/foo/bar"))
+  (is-true (http::path-prefix-p "/foo/" "/foo/bar"))
+  (is-true (http::path-prefix-p "/foo/bar" "/foo/bar"))
+  (is-true (http::path-prefix-p "/foo/bar" "/foo/bar/"))
+  (is-true (http::path-prefix-p "/foo/bar/" "/foo/bar/")))
+
+(test path-prefix-p/with-dots
+  (is-true (http::path-prefix-p "" "/.."))
+  (is-true (http::path-prefix-p "/" "/.."))
+  (is-true (http::path-prefix-p "foo" "/../foo"))
+  (is-true (http::path-prefix-p "foo" "/foo/."))
+  (is-true (http::path-prefix-p "foo/" "/.././foo"))
+  (is-true (http::path-prefix-p "/foo" "/../foo"))
+  (is-true (http::path-prefix-p "/foo/" "/../foo"))
+  (is-true (http::path-prefix-p "/foo" "/bar/../foo/"))
+  (is-true (http::path-prefix-p "/foo/" "/../foo/"))
+  (is-true (http::path-prefix-p "/foo" "/foo/bar/."))
+  (is-true (http::path-prefix-p "/foo/bar" "/foo/bar/."))
+  (is-true (http::path-prefix-p "/foo/bar" "/foo/bar/../bar")))
+
+(test resolve-path
+  #+os-windows
+  (it "should work on windows"
+      (is (equal "C:\\" (namestring (http::resolve-path "" "C:/" "/"))))
+      (is (equal "C:\\" (namestring (http::resolve-path "" "C:\\" "/"))))
+      (is (equal "C:\\abc" (namestring (http::resolve-path "" "C:/" "/abc"))))
+      (is (equal "C:\\abc" (namestring (http::resolve-path "" "C:\\" "/abc"))))
+      (is (equal "C:\\abc\\" (namestring (http::resolve-path "" "C:/" "/abc/"))))
+      (is (equal "C:\\abc\\def" (namestring (http::resolve-path "" "C:/" "/abc/def"))))
+      (is (equal "C:\\abc\\def\\" (namestring (http::resolve-path "" "C:/" "/abc/def/"))))
+      (is (equal "C:\\" (namestring (http::resolve-path "abc" "C:/" "/abc"))))
+      (is (equal "C:\\" (namestring (http::resolve-path "/abc" "C:/" "/abc/"))))
+      (is (equal "C:\\" (namestring (http::resolve-path "/abc/" "C:/" "/abc/"))))
+      (is (equal "C:\\def" (namestring (http::resolve-path "/abc/" "C:/" "/abc/def"))))
+      (is (equal "C:\\def\\" (namestring (http::resolve-path "/abc/" "C:/" "/abc/def/")))))
+  #-os-windows
+  (it "should work on linux"
+      ))
+
+(test resolve-path/with-dots
+  #+os-windows
+  (it "should work on windows"
+      (is (equal "C:\\" (namestring (http::resolve-path "" "C:/" "/.."))))
+      (is (equal "C:\\abc\\" (namestring (http::resolve-path "" "C:/" "/abc/."))))
+      (is (equal "C:\\abc\\" (namestring (http::resolve-path "" "C:/" "/abc/def/..")))))
+  #-os-windows
+  (it "should work on linux"
+      ))
+
+(test resolve-path/percent-encoding
+  #+os-windows
+  (it "should work on windows"
+      (is (equal "C:\\你好.txt" (namestring (http::resolve-path "" "C:/" "/%E4%BD%A0%E5%A5%BD.txt")))))
+  #-os-windows
+  (it "should work on linux"
+      ))
+
+(test static-route
+  (it
+    (let ((route (http::make-static-route :prefix "/" :root "/")))
+      (is-true (http::route-match-p route (make-instance 'request :method :get :uri "/")))
+      (is-false (http::route-match-p route (make-instance 'request :method :post :uri "/")))
+      (is-true (http::route-match-p route (make-instance 'request :method :get :uri "/foo"))))))
+
+(test static-handler
+  )
