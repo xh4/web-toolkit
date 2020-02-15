c8154f99aedaa76fe4590fde2e002cdec4e77b94
Xiangyu He <xh@coobii.com>
2020-02-15 16:02:16 +0800

    Add pathname-as-directory

--- http/directory.lisp
+++ http/directory.lisp
@@ -32,6 +32,19 @@
      (not (component-present-p (pathname-type pathspec)))
      pathspec)))
 
+(defun pathname-as-directory (pathspec)
+  (let ((pathname (pathname pathspec)))
+    (when (wild-pathname-p pathname)
+      (error "Can't reliably convert wild pathnames."))
+    (cond ((not (directory-pathname-p pathspec))
+           (make-pathname :directory (append (or (pathname-directory pathname)
+                                                 (list :relative))
+                                             (list (file-namestring pathname)))
+                          :name nil
+                          :type nil
+                          :defaults pathname))
+          (t pathname))))
+
 (defgeneric directory-modify-time (directory)
   (:method ((directory directory))
     (file-write-date (directory-pathname directory))))
