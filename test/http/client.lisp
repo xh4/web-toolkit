f9ec47b02ab493b714bd54d1f847b03bc134a567
Xiangyu He <xh@coobii.com>
2020-02-17 14:23:58 +0800

    Update client.lisp

--- test/http/client.lisp
+++ test/http/client.lisp
@@ -4,16 +4,20 @@
 
 (test client
   (it
-    (let ((res (request "http://example.com")))
-      (is (equal 200 (status-code res)))
-      (let ((body (http::read-response-body-into-string res)))
-        (is-true (stringp body))
-        (is-true (> (length body) 0))))))
+    (http::with-connection ("http://example.com")
+      (http::get "/")
+      (http::receive (res)
+        (is (equal 200 (status-code res)))
+        (let ((body (http::read-response-body-into-string res)))
+          (is-true (stringp body))
+          (is-true (> (length body) 0)))))))
 
 (test client/https
   (it
-    (let ((res (request "https://example.com")))
-      (is (equal 200 (status-code res)))
-      (let ((body (http::read-response-body-into-string res)))
-        (is-true (stringp body))
-        (is-true (> (length body) 0))))))
+    (http::with-connection ("https://example.com")
+      (http::get "/")
+      (http::receive (res)
+        (is (equal 200 (status-code res)))
+        (let ((body (http::read-response-body-into-string res)))
+          (is-true (stringp body))
+          (is-true (> (length body) 0)))))))
