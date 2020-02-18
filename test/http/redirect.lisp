843925fa14e5581f66ffe5cd0c772ebc0a37930e
Xiangyu He <xh@coobii.com>
2020-02-18 17:00:28 +0800

    Test redirect back

--- test/http/redirect.lisp
+++ test/http/redirect.lisp
@@ -17,3 +17,20 @@
                            (find-header-field
                             "Location"
                             res)))))))
+
+(test redirect-back
+  (it "should redirect back"
+      (define-handler foo ()
+        ()
+        (:function (lambda ()
+                     (redirect :back)
+                     (reply (status 200)))))
+
+      (let ((res (http::invoke-handler foo (make-instance 'request
+                                                          :header (header "Referer" "/foo")))))
+        (is (typep res 'response))
+        (is (equal 307 (status-code (response-status res))))
+        (is (equal "/foo" (header-field-value
+                           (find-header-field
+                            "Location"
+                            res)))))))
