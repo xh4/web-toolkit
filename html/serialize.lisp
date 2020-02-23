f9636024fbdbe04f5d0d2f167e66af123fdfaba9
Xiangyu He <xh@coobii.com>
2020-02-23 15:31:16 +0800

    Implement serialize

--- html/serialize.lisp
+++ html/serialize.lisp
@@ -1,170 +1,108 @@
 (in-package :html)
 
-;; (defclass sink (hax:abstract-handler)
-;;   ((stack
-;;     :initform nil
-;;     :accessor stack)
-;;    (stream
-;;     :initarg :stream
-;;     :initform nil
-;;     :accessor sink-stream)))
+(define-condition event ()
+  ((type
+    :initarg :type
+    :initform nil
+    :accessor event-type)
+   (node
+    :initarg :node
+    :initform nil
+    :accessor event-node)))
 
-;; (defmethod hax:start-document ((sink sink) name public-id system-id)
-;;   (declare (ignore name public-id system-id))
-;;   (write-string "<!DOCTYPE html>" (sink-stream sink)))
+(defun traverse (node)
+  (when (null node) (return-from traverse nil))
+  (let ((stack nil))
+    (loop for node in (reverse (ensure-list node))
+       do (push `(:enter . ,node) stack))
+    (loop for (action . node) = (pop stack)
+       while node
+       do
+         (case action
+           (:enter (typecase node
+                     (text (signal 'event :type :enter :node node))
+                     ((or element document) (signal 'event :type :enter :node node)
+                      (push `(:leave . ,node) stack)
+                      (loop for child in (reverse (dom:children node))
+                         do (push `(:enter . ,child) stack)))))
+           (:leave (typecase node
+                     (element
+                      (signal 'event :type :leave :node node))))))))
 
-;; (defmethod hax:end-document ((sink sink)))
+(defun write-char (char &optional (stream *standard-output*) &key)
+  (cl:write-char char stream))
 
-;; (defmethod hax:start-element ((sink sink) name attributes)
-;;   (let* ((key (find-symbol (string-upcase name) :keyword))
-;; 	 (elt
-;; 	  (and key (sgml::find-element closure-html::*html-dtd* key nil nil)))
-;; 	 (attlist (and elt (sgml::element-attlist elt))))
-;;     (push (cons name elt) (stack sink))
-;;     (write-char #\< (sink-stream sink))
-;;     (write-string name (sink-stream sink))
-;;     (dolist (a attributes)
-;;       (let* ((aname (hax:attribute-name a))
-;; 	     (akey (find-symbol (string-upcase aname) :keyword))
-;; 	     (att (and akey (assoc akey attlist)))
-;; 	     (values (second att)))
-;; 	(write-char #\space (sink-stream sink))
-;; 	(write-string aname (sink-stream sink))
-;;         (let ((value (hax:attribute-value a)))
-;;           (when (> (length value) 0)
-;;             (unless (and att (listp values) (eq (car att) (car values)))
-;;               (write-char #\= (sink-stream sink))
-;;               (write-char #\" (sink-stream sink))
-;;               (when (uri-attribute-p name aname)
-;;                 (setf value (escape-uri-attribute value)))
-;;               (write-attribute-string value sink)
-;;               (write-char #\" (sink-stream sink)))))))
-;;     (write-char #/> (sink-stream sink))))
+(defun write-string (string &optional (stream *standard-output*) &key)
+  (cl:write-string string stream))
 
-;; (defun uri-attribute-p (ename aname)
-;;   (find (string-downcase aname)
-;; 	(cdr (find (string-downcase ename)
-;; 		   '((#"a"          #"href" #"name")
-;; 		     (#"area"       #"href")
-;; 		     (#"link"       #"href")
-;; 		     (#"img"        #"src" #"longdesc" #"usemap")
-;; 		     (#"object"     #"classid" #"codebase" #"data" #"usemap")
-;; 		     (#"q"          #"cite")
-;; 		     (#"blockquote" #"cite")
-;; 		     (#"inl"        #"cite")
-;; 		     (#"del"        #"cite")
-;; 		     (#"form"       #"action")
-;; 		     (#"input"      #"src" #"usemap")
-;; 		     (#"head"       #"profile")
-;; 		     (#"base"       #"href")
-;; 		     (#"script"     #"src"  ;; #"for"
-;; 		      ))
-;; 		   :key #'car
-;; 		   :test #'string=))
-;; 	:test #'string=))
+(defun write-text (text &optional (stream *standard-output*) &key)
+  (let ((parent (dom:parent text)))
+    (etypecase parent
+      (normal-element (write-normal-text text stream))
+      (escapable-raw-text-element (write-escapable-raw-text text stream)))))
 
-;; (defun escape-uri-attribute (x)
-;;   (with-output-to-string (s)
-;;     (loop
-;;        for c across x
-;;        for code = (char-code c)
-;;        do
-;; 	 (if (< code 128)
-;; 	     (write-char c s)
-;; 	     (format s "%~2,'0X" code)))))
+(defun write-normal-text (text &optional (stream *standard-output*) &key)
+  (loop for char across (dom:data text)
+     do (cond
+          ((char= char #\<) (write-string "&lt;" stream))
+          (t (write-char char stream)))))
 
-;; (defmethod hax:end-element
-;;     ((sink sink) name)
-;;   (let* ((prev (pop (stack sink)))
-;; 	 (prev-name (car prev))
-;; 	 (elt (cdr prev)))
-;;     (unless (string= prev-name name)
-;;       (error "output does not nest: expected ~A but got ~A"
-;;              name prev-name))
-;;     (unless (and elt (null (sgml::element-include elt)))
-;;       (write-string "</" (sink-stream sink))
-;;       (write-string name (sink-stream sink))
-;;       (write-string ">" (sink-stream sink)))))
+(defun write-escapable-raw-text (text &optional (stream *standard-output*) &key)
+  (loop for char across (dom:data text)
+     do (cond ((char= char #\Tab) (write-string "&#9;" stream))
+              ((char= char #\Newline) (write-string "&#10;" stream))
+              ((char= char #\Return) (write-string "&#13;" stream))
+              ((char= char #\&) (write-string "&amp;" stream))
+              ((char= char #\<) (write-string "&lt;" stream))
+              ((char= char #\>) (write-string "&gt;" stream))
+              ((char= char #\") (write-string "&quot;" stream))
+              (t (write-char char stream)))))
 
-;; (defmethod hax:characters ((sink sink) data)
-;;   (let ((stream (sink-stream sink)))
-;;     (if (find (caar (stack sink)) '("script" "style") :test 'equalp)
-;; 	(write-string data (sink-stream sink))
-;; 	(loop for c across data do (write-datachar-readable c (sink-stream sink))))))
+(defun write-element-start-tag (element &optional (stream *standard-output*) &key)
+  (write-string "<" stream)
+  (write-string (dom:tag-name element) stream)
+  (loop for name in (dom:get-attribute-names element)
+     for value = (dom:get-attribute element name)
+     do (write-attribute name value stream))
+  (write-string ">" stream))
 
-;; (defun write-attribute-string (str sink)
-;;   (let ((stream (sink-stream sink)))
-;;     (loop
-;;        for i from 1
-;;        for c across str
-;;        do
-;; 	 (cond ((char= c #\&)
-;; 		(if (and (< i (length str)) (char= (char str i) #/{))
-;; 		    (write-char c stream)
-;;                     (write-string "&amp;" stream)))
-;; 	       ((char= c #\") (write-string "&quot;" stream))
-;; 	       ((char= c #\Newline) (write-string "&#10;" stream))
-;; 	       ((char= c #\Return) (write-string "&#13;" stream))
-;; 	       (t (write-char c stream))))))
+(defun write-attribute (name value &optional (stream *standard-output*) &key)
+  (when value
+    (write-char #\space stream)
+    (write-string name stream)
+    (unless (eq t value)
+      (write-char #\= stream)
+      (write-double-quoted-attribute-value value stream))))
 
-;; (defun write-datachar (char stream)
-;;   (cond ((char= char #\&) (write-string "&amp;" stream))
-;;         ((char= char #\<) (write-string "&lt;" stream))
-;;         ((char= char #\>) (write-string "&gt;" stream))
-;;         ((char= char #\") (write-string "&quot;" stream))
-;;         ((char= char #\Tab) (write-string "&#9;" stream))
-;;         ((char= char #\Newline) (write-string "&#10;" stream))
-;;         ((char= char #\Return) (write-string "&#13;" stream))
-;;         (t (write-char char stream))))
+(defun write-double-quoted-attribute-value (value stream)
+  (write-char #\" stream)
+  (loop for char across value
+     do (cond
+          ((char= char #\") (write-string "&quot;" stream))
+          (t (write-char char stream))))
+  (write-char #\" stream))
 
-;; (defun write-datachar-readable (char stream)
-;;   (cond ((char= char #\&) (write-string "&amp;" stream))
-;;         ((char= char #\<) (write-string "&lt;" stream))
-;;         ((char= char #\>) (write-string "&gt;" stream))
-;;         ((char= char #\") (write-string "&quot;" stream))
-;;         ((char= char #\Return) (write-string "&#13;" stream))
-;;         (t (write-char char stream))))
+(defun write-element-end-tag (element &optional (stream *standard-output*) &key)
+  (unless (typep element 'void-element)
+    (write-string "</" stream)
+    (write-string (dom:tag-name element) stream)
+    (write-string ">" stream)))
 
-;; (defun serialize (root &optional (stream nil))
-;;   (let ((handler nil))
-;;     (labels ((serialize-to-stream (root stream)
-;;                (setf handler (make-instance 'sink :stream stream))
-;;                (typecase root
-;;                  (document
-;;                   (hax:start-document handler nil nil nil)
-;;                   (dom:do-node-list (child (dom:child-nodes root))
-;;                     (visit child))
-;;                   (hax:end-document handler))
-;;                  ((or element text)
-;;                   (visit root))
-;;                  (t (error "Can't serialize ~A of type ~A" root (type-of root)))))
-;;              (visit (node)
-;;                (ecase (dom:node-type node)
-;;                  (:element
-;;                   (let ((name (dom:tag-name node))
-;;                         (attributes (compute-attributes node)))
-;;                     (hax:start-element handler name attributes)
-;;                     (walk node)
-;;                     (hax:end-element handler name)))
-;;                  (:text
-;;                   (hax:characters handler (dom:data node)))))
-;;              (walk (node)
-;;                (dom:do-node-list (child (dom:child-nodes node))
-;;                  (visit child))))
-;;       (if (eq t stream)
-;;           (progn (serialize-to-stream root *standard-output*) nil)
-;;           (typecase stream
-;;             (null (with-output-to-string (stream)
-;;                     (serialize-to-stream root stream)))
-;;             (t (progn (serialize-to-stream root *standard-output*) nil)))))))
+(defun write-doctype (document &optional (stream *standard-output*) &key)
+  (declare (ignore document))
+  (write-string "<!DOCTYPE html>" stream))
 
-;; (defun compute-attributes (element)
-;;   (let ((results '()))
-;;     (dom:do-node-list (a (dom:attributes element))
-;;       (when (dom:value a)
-;;         (push
-;;          (hax:make-attribute (dom:name a)
-;;                              (dom:value a)
-;;                              (dom:specified a))
-;;          results)))
-;;     (reverse results)))
+(defun serialize (root &optional (stream *standard-output*))
+  (handler-bind
+      ((event
+        (lambda (event)
+          (with-slots (type node) event
+            (case type
+              (:enter (typecase node
+                        (document (write-doctype node stream))
+                        (element (write-element-start-tag node stream))
+                        (text (write-text node stream))))
+              (:leave (unless (typep node 'void-element)
+                        (typecase node
+                          (element (write-element-end-tag node stream))))))))))
+    (traverse root)))
