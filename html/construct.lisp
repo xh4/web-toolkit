1afcd1b86cd9ec097768c19d682e70db5e4f3977
Xiangyu He <xh@coobii.com>
2020-02-24 22:57:42 +0800

    Add function to obtain constructor

--- html/construct.lisp
+++ html/construct.lisp
@@ -3,6 +3,13 @@
 (defclass constructor () ())
 
 (defmethod print-object ((constructor constructor) stream)
-  (print-unreadable-object (constructor stream :type t)))
+  (print-unreadable-object (constructor stream :type t :identity t)))
 
 (defgeneric construct (constructor &rest arguments))
+
+(defun constructor (tag-name)
+  (let ((symbol (typecase tag-name
+                  (symbol (find-symbol (symbol-name tag-name) :html))
+                  (string (find-symbol (string-upcase tag-name) :html)))))
+    (when (and (boundp symbol) (typep (symbol-value symbol) 'constructor))
+      (symbol-value symbol))))
