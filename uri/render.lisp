b605631d75acc72af6e0816d9e1a83df00db9947
Xiangyu He <xh@coobii.com>
2020-02-25 17:18:49 +0800

    Add render-uri

--- uri/render.lisp
+++ uri/render.lisp
@@ -1,68 +1,72 @@
 (in-package :uri)
 
+(defun render-uri (uri)
+  (check-type uri uri)
+  (if-let ((uri-string (slot-value uri 'string)))
+    uri-string
+    (let ((scheme (uri-scheme uri))
+          (host (uri-host uri))
+          (userinfo (uri-userinfo uri))
+          (port (uri-port uri))
+          (path (uri-path uri))
+          (query (uri-query uri))
+          (fragment (uri-fragment uri)))
+      (let ((uri-string
+             (concatenate 'string
+                          (when scheme
+                            scheme)
+                          (when scheme ":")
+                          (when host
+                            "//")
+                          (when userinfo
+                            (percent-encode
+                             userinfo
+                             :reserve (lambda (c)
+                                        (or (unreserved-p c)
+                                            (sub-delim-p c)
+                                            (eq c #\:)))))
+                          (when userinfo
+                            "@")
+                          (when host
+                            (percent-encode
+                             host
+                             :reserve (lambda (c)
+                                        (or (unreserved-p c)
+                                            (sub-delim-p c)))))
+                          (when port
+                            ":")
+                          (when port
+                            (format nil "~A" port))
+                          (when path
+                            (percent-encode
+                             path
+                             :reserve (lambda (c)
+                                        (or (unreserved-p c)
+                                            (sub-delim-p c)
+                                            (eq c #\:)
+                                            (eq c #\@)
+                                            (eq c #\/)))))
+                          (when query
+                            "?")
+                          (when query
+                            query)
+                          (when fragment
+                            "#")
+                          (when fragment
+                            (percent-encode
+                             fragment
+                             :reserve (lambda (c)
+                                        (or (unreserved-p c)
+                                            (sub-delim-p c)
+                                            (eq c #\:)
+                                            (eq c #\@)
+                                            (eq c #\/)
+                                            (eq c #\?))))))))
+        (setf (slot-value uri 'string) uri-string)))))
+
 (defgeneric uri-string (uri)
   (:method ((uri uri))
-    (if-let ((uri-string (slot-value uri 'string)))
-      uri-string
-      (let ((scheme (uri-scheme uri))
-            (host (uri-host uri))
-            (userinfo (uri-userinfo uri))
-            (port (uri-port uri))
-            (path (uri-path uri))
-            (query (uri-query uri))
-            (fragment (uri-fragment uri)))
-        (let ((uri-string
-               (concatenate 'string
-                            (when scheme
-                              scheme)
-                            (when scheme ":")
-                            (when host
-                              "//")
-                            (when userinfo
-                              (percent-encode
-                               userinfo
-                               :reserve (lambda (c)
-                                          (or (unreserved-p c)
-                                              (sub-delim-p c)
-                                              (eq c #\:)))))
-                            (when userinfo
-                              "@")
-                            (when host
-                              (percent-encode
-                               host
-                               :reserve (lambda (c)
-                                          (or (unreserved-p c)
-                                              (sub-delim-p c)))))
-                            (when port
-                              ":")
-                            (when port
-                              (format nil "~A" port))
-                            (when path
-                              (percent-encode
-                               path
-                               :reserve (lambda (c)
-                                          (or (unreserved-p c)
-                                              (sub-delim-p c)
-                                              (eq c #\:)
-                                              (eq c #\@)
-                                              (eq c #\/)))))
-                            (when query
-                              "?")
-                            (when query
-                              query)
-                            (when fragment
-                              "#")
-                            (when fragment
-                              (percent-encode
-                               fragment
-                               :reserve (lambda (c)
-                                          (or (unreserved-p c)
-                                              (sub-delim-p c)
-                                              (eq c #\:)
-                                              (eq c #\@)
-                                              (eq c #\/)
-                                              (eq c #\?))))))))
-          (setf (slot-value uri 'string) uri-string))))))
+    (render-uri uri)))
 
 (defmethod print-object ((uri uri) stream)
   (print-unreadable-object (uri stream :type t :identity t)
