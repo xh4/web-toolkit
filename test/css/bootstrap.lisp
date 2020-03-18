4281952630832f806425b1374086f575b1b2cf8f
Xiangyu He <xh@coobii.com>
2020-03-18 18:22:34 +0800

    Fix test generations

--- test/css/bootstrap.lisp
+++ test/css/bootstrap.lisp
@@ -2,37 +2,38 @@
 
 (in-suite :css-test)
 
-(defun get-bootstrap ()
-  (let* ((response (http:get "https://cdn.bootcss.com/twitter-bootstrap/4.4.1/css/bootstrap.css"))
-         (octets (alexandria::read-stream-content-into-byte-vector
-                  (http:response-body response))))
-    (babel:octets-to-string octets)))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun get-bootstrap ()
+    (let* ((response (http:get "https://cdn.bootcss.com/twitter-bootstrap/4.4.1/css/bootstrap.css"))
+           (octets (alexandria::read-stream-content-into-byte-vector
+                    (http:response-body response))))
+      (babel:octets-to-string octets)))
 
-(defun bootstrap-rules ()
-  (let ((code (get-bootstrap)))
-    (let ((code-without-comments (with-output-to-string (output)
-                                   (with-input-from-string (input code)
-                                     (loop for line = (read-line input nil nil)
-                                        while line
-                                        unless (cl-ppcre:scan "^/|\\s+\\*" line)
-                                        do (write-line line output))))))
-      (let ((rules (cl-ppcre:split "\\n\\n" code-without-comments)))
-        (loop for rule in rules
-           unless (cl-ppcre:scan "^@" rule)
-           collect rule)))))
+  (defun bootstrap-rules ()
+    (let ((code (get-bootstrap)))
+      (let ((code-without-comments (with-output-to-string (output)
+                                     (with-input-from-string (input code)
+                                       (loop for line = (read-line input nil nil)
+                                          while line
+                                          unless (cl-ppcre:scan "^/|\\s+\\*" line)
+                                          do (write-line line output))))))
+        (let ((rules (cl-ppcre:split "\\n\\n" code-without-comments)))
+          (loop for rule in rules
+             unless (cl-ppcre:scan "^@" rule)
+             collect rule)))))
 
-(defun bootstrap-declarations ()
-  (loop for rule in (bootstrap-rules)
-     do (setf rule (cl-ppcre:regex-replace-all "\\n" rule " "))
-     append (let ((body (first
-                          (coerce
-                           (nth-value 1 (cl-ppcre:scan-to-strings ".*{(.*)}" rule))
-                           'list))))
-               (loop for dec in (cl-ppcre:split "\\s*;\\s*" body)
-                  for declaration = (string-trim '(#\Space) dec)
-                  for (name value) = (cl-ppcre:split "\\s*:\\s*" declaration)
-                  unless (cl-ppcre:scan "^-" name)
-                  collect `(,name . ,value)))))
+  (defun bootstrap-declarations ()
+    (loop for rule in (bootstrap-rules)
+       do (setf rule (cl-ppcre:regex-replace-all "\\n" rule " "))
+       append (let ((body (first
+                           (coerce
+                            (nth-value 1 (cl-ppcre:scan-to-strings ".*{(.*)}" rule))
+                            'list))))
+                (loop for dec in (cl-ppcre:split "\\s*;\\s*" body)
+                   for declaration = (string-trim '(#\Space) dec)
+                   for (name value) = (cl-ppcre:split "\\s*:\\s*" declaration)
+                   unless (cl-ppcre:scan "^-" name)
+                   collect `(,name . ,value))))))
 
 (defmacro test-bootstrap ()
   `(progn
