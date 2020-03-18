327304b0f54f370581af04a5a183f22f5c54130e
Xiangyu He <xh@coobii.com>
2020-03-18 18:22:08 +0800

    Add what-to-implement

--- test/css/helper.lisp
+++ test/css/helper.lisp
@@ -1,1 +1,13 @@
 (in-package :css-test)
+
+(defun what-to-implement ()
+  (remove-duplicates
+   (loop for (name . value) in (bootstrap-declarations)
+      for index from 1
+      append (let* ((symbol (find-symbol (string-upcase name) :css))
+                    (function (when symbol
+                                (ignore-errors
+                                  (symbol-function symbol)))))
+               (unless function
+                 (list name))))
+   :test 'equal))
