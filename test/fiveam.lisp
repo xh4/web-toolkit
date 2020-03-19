e8f778f18ce84b20608a0ae9ec5cc37ee3059ce6
Xiangyu He <xh@coobii.com>
2020-03-19 12:40:56 +0800

    Report context on error

--- test/fiveam.lisp
+++ test/fiveam.lisp
@@ -54,4 +54,84 @@
              :reason reason))
     (add-result 'test-failure :test-expr test-expr
                 :reason reason)))
+
+(defmethod run-test-lambda ((test test-case))
+  (with-run-state (result-list)
+    (bind-run-state ((current-test test))
+      (labels ((abort-test (e &optional (reason (format nil "Unexpected Error: ~S~%~A." e e)))
+                 (setf test:*it*
+                       (test::map-tree
+                        (lambda (form)
+                          (if (and (listp form)
+                                   (not (emptyp form))
+                                   (equal (first form) 'test:=>))
+                              (throw :skip
+                                (if (equal (second test:*is*) (second form))
+                                    (cons 'test:=> (cddr form))
+                                    (cddr form)))
+                              form))
+                        test:*it*
+                        :tag :skip))
+                 (let ((reason (with-output-to-string (stream)
+                                 (format stream "~%IN")
+                                 (loop for form in test:*it*
+                                    do (format stream "~%")
+                                      (pprint form stream))
+                                 (format stream "~%~%~A" reason))))
+                   (add-result 'unexpected-test-failure
+                               :test-expr nil
+                               :test-case test
+                               :reason reason
+                               :condition e)))
+               (run-it ()
+                 (let ((result-list '()))
+                   (declare (special result-list))
+                   (handler-bind ((check-failure (lambda (e)
+                                                   (declare (ignore e))
+                                                   (cond
+                                                     ((eql *on-failure* :debug)
+                                                      nil)
+                                                     (t
+                                                      (when (eql *on-failure* :backtrace)
+                                                        (trivial-backtrace:print-backtrace-to-stream
+                                                         *test-dribble*))
+                                                      (invoke-restart
+                                                       (find-restart 'ignore-failure))))))
+                                  (error (lambda (e)
+                                           (unless (or (eql *on-error* :debug)
+                                                       (typep e 'check-failure))
+                                             (when (eql *on-error* :backtrace)
+                                               (trivial-backtrace:print-backtrace-to-stream
+                                                *test-dribble*))
+                                             (abort-test e)
+                                             (return-from run-it result-list)))))
+                     (restart-case
+                         (handler-case
+                             (let ((*readtable* (copy-readtable))
+                                   (*package* (runtime-package test)))
+                               (when *print-names*
+                                 (format *test-dribble* "~%~ARunning test ~A " *test-dribble-indent* (name test)))
+                               (if (collect-profiling-info test)
+                                   ;; Timing info doesn't get collected ATM, we need a portable library
+                                   ;; (setf (profiling-info test) (collect-timing (test-lambda test)))
+                                   (funcall (test-lambda test))
+                                   (funcall (test-lambda test))))
+                           (storage-condition (e)
+                             ;; heap-exhausted/constrol-stack-exhausted
+                             ;; handler-case unwinds the stack (unlike handler-bind)
+                             (abort-test e (format nil "STORAGE-CONDITION: aborted for safety. ~S~%~A." e e))
+                             (return-from run-it result-list)))
+                       (retest ()
+                         :report (lambda (stream)
+                                   (format stream "~@<Rerun the test ~S~@:>" test))
+                         (return-from run-it (run-it)))
+                       (ignore ()
+                         :report (lambda (stream)
+                                   (format stream "~@<Signal an exceptional test failure and abort the test ~S.~@:>" test))
+                         (abort-test (make-instance 'test-failure :test-case test
+                                                    :reason "Failure restart."))))
+                     result-list))))
+        (let ((results (run-it)))
+          (setf (status test) (results-status results)
+                result-list (nconc result-list results)))))))
 (in-package :test)
