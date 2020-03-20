3b160815cd7e164217b4ff4c3c51a81162ef18a1
Xiangyu He <xh@coobii.com>
2020-03-20 17:03:31 +0800

    Avoid cycle

--- reactive/propagate.lisp
+++ reactive/propagate.lisp
@@ -1,13 +1,18 @@
 (in-package :reactive)
 
 (defun propagate-0 (object record)
-  (let ((*local-propagation* nil)
-        (*local-propagation-p* t))
-    (let ((*record* record))
-      ;; cycle
-      (unless (eq object (record-object record))
-        (react object (record-object record))))
-    *local-propagation*))
+  (labels ((detect-cycle (object record)
+             (or (eq object (record-object record))
+                 (and (record-previous record)
+                      (detect-cycle object (record-previous record))))))
+    (let ((*local-propagation* nil)
+          (*local-propagation-p* t))
+      (let ((*record* record))
+        ;; cycle
+        (if (detect-cycle object record)
+            (format t "Cycle on ~A~%" object)
+            (react object (record-object record))))
+      *local-propagation*)))
 
 (defun propagate-1 ()
   (when-let ((propagation (pop *global-propagation*)))
@@ -28,13 +33,3 @@
 (defmacro with-propagation (&body body)
   `(let ((*propagation-p* t))
      ,@body))
-
-(define-reactive-class myclass ()
-  ((a :initform 1)))
-
-(let ((o1 (make-instance 'myclass))
-      (o2 (make-instance 'myclass))
-      (o3 (make-instance 'myclass)))
-  (add-dependency o2 o1)
-  (add-dependency o3 o2)
-  (setf (slot-value o1 'a) 2))
