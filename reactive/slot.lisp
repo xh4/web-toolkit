63b1e6a5b8b283056b8b23c239636662d9c70296
Xiangyu He <xh@coobii.com>
2020-03-23 21:18:58 +0800

    Ignore certain slots

--- reactive/slot.lisp
+++ reactive/slot.lisp
@@ -4,22 +4,28 @@
   (call-next-method))
 
 (defmethod (setf slot-value-using-class) :around (value (class reactive-class) object slot)
+  ;; (when *propagation-p*
+  ;;   (typecase slot
+  ;;     (symbol slot)
+  ;;     (slot-definition (setf slot (slot-definition-name slot))))
+  ;;   (format t "Set slot ~A of ~A (with propagation)~%" slot object))
   (when *propagation-p*
     (typecase slot
       (symbol slot)
       (slot-definition (setf slot (slot-definition-name slot))))
-    (let ((current-value (slot-value object slot)))
-      (unless (eq value current-value)
-        (let ((record (make-instance 'record
-                                     :object object
-                                     :slot slot
-                                     :previous-value current-value
-                                     :current-value value
-                                     :previous *record*)))
-          (loop for o in (object-propagation object)
-             do (if *local-propagation-p*
-                    (push (cons o record) *local-propagation*)
-                    (push (cons o record) *global-propagation*)))))))
+    (unless (member slot '(dependency propagation))
+      (let ((current-value (slot-value object slot)))
+        (unless (eq value current-value)
+          (let ((record (make-instance 'record
+                                       :object object
+                                       :slot slot
+                                       :previous-value current-value
+                                       :current-value value
+                                       :previous *record*)))
+            (loop for o in (object-propagation object)
+               do (if *local-propagation-p*
+                      (push (cons o record) *local-propagation*)
+                      (push (cons o record) *global-propagation*))))))))
   (prog1
       (call-next-method)
     (when (and *propagation-p*
