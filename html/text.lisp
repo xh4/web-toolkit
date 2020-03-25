8463bc901b50caa1711590e3ccc6ee6d80d1dcd9
Xiangyu He <xh@coobii.com>
2020-03-25 23:46:12 +0800

    Not print text data

--- html/text.lisp
+++ html/text.lisp
@@ -3,10 +3,7 @@
 (defclass text (dom:text) ())
 
 (defmethod print-object ((text text) stream)
-  (print-unreadable-object (text stream :type t :identity t)
-    (let ((data (dom:data text)))
-      (when data
-        (format stream "~S" data)))))
+  (print-unreadable-object (text stream :type t :identity t)))
 
 (defun text (&optional data)
   (check-type data (or null string))
