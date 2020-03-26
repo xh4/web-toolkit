d21ed66d4d2ae5f8063a1cd588073f66a8175eff
Xiangyu He <xh@coobii.com>
2020-03-26 14:43:01 +0800

    Add render table

--- component/render.lisp
+++ component/render.lisp
@@ -1,17 +1,21 @@
 (in-package :component)
 
 (defun render-all (node)
-  (typecase node
-    (html:text node)
-    (string (html:text node))
-    (component
-     (setf node (render node))
-     (loop for i from 0
-        for child in (dom:children node)
-        do (setf (nth i (dom:children node)) (render-all child))
-        finally (return node)))
-    (html:element
-     (loop for i from 0
-        for child in (dom:children node)
-        do (setf (nth i (dom:children node)) (render-all child))
-        finally (return node)))))
+  (let ((table (make-hash-table)))
+    (labels ((render-1 (node table)
+               (typecase node
+                 (html:text node)
+                 (string (html:text node))
+                 (component
+                  (let ((root (render node)))
+                    (setf (gethash node table) root)
+                    (loop for i from 0
+                       for child in (dom:children root)
+                       do (setf (nth i (dom:children root)) (render-1 child table))
+                       finally (return root))))
+                 (html:element
+                  (loop for i from 0
+                     for child in (dom:children node)
+                     do (setf (nth i (dom:children node)) (render-1 child table))
+                     finally (return node))))))
+      (values (render-1 node table) table))))
