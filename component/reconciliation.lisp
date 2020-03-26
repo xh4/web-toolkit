49bd6bb7ecfcdfb66b90cbf975da4d70c4531085
Xiangyu He <xh@coobii.com>
2020-03-26 16:17:04 +0800

    Diff element attributes

--- component/reconciliation.lisp
+++ component/reconciliation.lisp
@@ -8,14 +8,30 @@
 ;; (diff (html:h1) (html:h3))
 ;; (diff (html:h1 (html:h2) (html:h4)) (html:h1 (html:h3)))
 ;; (diff (html:h1 "foo") (html:h1 "bar"))
+;; (diff (html:h1 :class "aaa" "foo") (html:h1 :class "bbb" "bar"))
 
 (defvar *diff-path* nil)
 
 (defgeneric diff-node (a b)
   (:method ((a html:element) (b html:element))
     (if (equal (dom:tag-name a) (dom:tag-name b))
-        (when (or (dom:children a) (dom:children b))
-          (diff-children a b))
+        (let ((a-attributes (slot-value a 'dom::attributes))
+              (b-attributes (slot-value b 'dom::attributes)))
+          (let ((attributes '()))
+            (loop for (a-name . a-value) in a-attributes
+               for (b-name . b-value) = (assoc a-name b-attributes :test 'equal)
+               do (if b-name
+                      (unless (equal a-value b-value)
+                        (push `(,b-name . ,b-value) attributes))
+                      (push `(,a-name . nil) attributes)))
+            (loop for (b-name . b-value) in b-attributes
+               for (a-name . nil) = (assoc b-name a-attributes :test 'equal)
+               unless a-name
+               do (push `(,b-name . ,b-value) attributes))
+            (append (when attributes
+                      `((:update ,*diff-path* ,a ,attributes)))
+                    (when (or (dom:children a) (dom:children b))
+                      (diff-children a b)))))
         `((:replace ,*diff-path* ,a ,b))))
   (:method ((a html:text) (b html:text))
     (unless (equal (dom:data a) (dom:data b))
