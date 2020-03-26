9bccbafa3f9b5e658622a18cd98a5c90ec42c669
Xiangyu He <xh@coobii.com>
2020-03-26 16:44:24 +0800

    Test update

--- live/test.lisp
+++ live/test.lisp
@@ -1,23 +1,29 @@
 (in-package :live)
 
-(progn
-  (define-page test-page ()
-    ())
+(define-page test-page ()
+  ())
 
-  (com:define-component test-component ()
-    ()
-    (:render
-     (lambda ()
-       (html:h1 "Test Page 3"))))
+(com:define-component test-component ()
+  ()
+  (:render
+   (lambda ()
+     (html:div :style "color: red"
+              :class "foo bar"
+              "Test Page 1"
+              (html:ul
+               (html:li "000")
+               (html:li "111")
+               (html:li "222")
+               (html:li "333"))))))
 
-  (defmethod page-content ((page test-page))
-    (test-component))
+(defmethod page-content ((page test-page))
+  (test-component))
 
-  (http:define-server test-server ()
-    ()
-    (:listener (http:listener :port 4001))
-    (:handler (router
-               (:page test-page)))))
+(http:define-server test-server ()
+  ()
+  (:listener (http:listener :port 4001))
+  (:handler (router
+             (:page test-page))))
 
 ;; (http:start-server test-server)
 ;; (http:stop-server test-server)
