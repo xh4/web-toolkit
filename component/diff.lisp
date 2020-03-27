5d2abaf1caccb05e6ff3e5ee2d4c31b2e2346ce5
Xiangyu He <xh@coobii.com>
2020-03-27 12:25:24 +0800

    Code refactoring

--- component/diff.lisp
+++ component/diff.lisp
@@ -5,11 +5,6 @@
   (check-type b html:element)
   (diff-node a b))
 
-;; (diff (html:h1) (html:h3))
-;; (diff (html:h1 (html:h2) (html:h4)) (html:h1 (html:h3)))
-;; (diff (html:h1 "foo") (html:h1 "bar"))
-;; (diff (html:h1 :class "aaa" "foo") (html:h1 :class "bbb" "bar"))
-
 (defvar *diff-path* nil)
 
 (defgeneric diff-node (a b)
@@ -46,21 +41,16 @@
         (b-classes (split-sequence #\Space b-class)))
     (equal (sort a-classes #'string<) (sort b-classes #'string<))))
 
-;; (class-equal "foo bar" "bar foo")
-
 (defun diff-children (a b)
   (let ((a-children (dom:children a))
         (b-children (dom:children b)))
-    (let ((length (max (length a-children) (length b-children))))
-      (appendf a-children (loop repeat (- length (length a-children)) collect nil))
-      (appendf b-children (loop repeat (- length (length b-children)) collect nil))
-      (loop for index from 0
-         for a-child in a-children
-         for b-child in b-children
-         append (cond
-                  ((and a-child (not b-child)) `((:remove ,(append *diff-path* `(,index)) ,a-child)))
-                  ((and b-child (not a-child)) `((:insert ,*diff-path* ,a ,index ,b-child)))
-                  (t (let ((*diff-path* (append *diff-path* `(,index))))
-                       (diff-node a-child b-child))))))))
-
-;; (diff-children (html:p (html:h1) (html:h2)) (html:p (html:h3) (html:h4) (html:h5)))
+    (loop for index from 0 to (max (length a-children) (length b-children))
+       for a-child = (nth index a-children)
+       for b-child = (nth index b-children)
+       append (cond
+                ((and a-child (not b-child))
+                 `((:remove ,(append *diff-path* `(,index)) ,a-child)))
+                ((and (not a-child) b-child)
+                 `((:insert ,*diff-path* ,a ,index ,b-child)))
+                (t (let ((*diff-path* (append *diff-path* `(,index))))
+                     (diff-node a-child b-child)))))))
