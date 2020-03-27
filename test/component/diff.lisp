0fe1e57f9a07227ff88bcbdf42b5dd5ed064ed4c
Xiangyu He <xh@coobii.com>
2020-03-27 13:19:03 +0800

    Update tests

--- test/component/diff.lisp
+++ test/component/diff.lisp
@@ -3,53 +3,94 @@
 (in-suite :component-test)
 
 (test diff
-  (is (equal nil (com::diff (html:div) (html:div))))
+  (is (equal nil (diff (div) (div))))
 
-  (is (equal nil (com::diff (html:div :class "foo") (html:div :class "foo"))))
+  (is (equal nil (diff (div :class "foo") (div :class "foo"))))
 
-  (is (equal nil (com::diff (html:div :class "foo bar") (html:div :class "bar foo")))))
+  (is (equal nil (diff (div :class "foo bar") (div :class "bar foo")))))
 
 (test diff/update
   (it
-    (let ((a (html:div :class "foo"))
-          (b (html:div :class "bar")))
-      (is (equal `((:update nil ,a (("class" . "bar")))) (com::diff a b)))))
+    (let ((a (div :class "foo"))
+          (b (div :class "bar")))
+      (is (equal `((:update nil ,a (("class" . "bar")))) (diff a b)))))
 
   (it
-    (let ((a (html:div :class "foo"))
-          (b (html:div)))
-      (is (equal `((:update nil ,a (("class" . nil)))) (com::diff a b)))))
+    (let ((a (div :class "foo"))
+          (b (div)))
+      (is (equal `((:update nil ,a (("class" . nil)))) (diff a b)))))
 
   (it
-    (let ((a (html:div))
-          (b (html:div :class "bar")))
-      (is (equal `((:update nil ,a (("class" . "bar")))) (com::diff a b))))))
+    (let ((a (div))
+          (b (div :class "bar")))
+      (is (equal `((:update nil ,a (("class" . "bar")))) (diff a b))))))
 
 (test diff/replace
   (it
-    (let ((a (html:h1))
-          (b (html:h2)))
-      (is (equal `((:replace nil ,a ,b)) (com::diff a b)))))
+    (let ((a (h1))
+          (b (h2)))
+      (is (equal `((:replace nil ,a ,b)) (diff a b)))))
 
   (it
-    (let ((a (html:h1))
-          (b (html:text)))
-      (is (equal `((:replace (0) ,a ,b)) (com::diff (html:div a) (html:div b))))))
+    (let ((a (h1))
+          (b (text)))
+      (is (equal `((:replace (0) ,a ,b)) (diff (div a) (div b))))))
 
   (it
-    (let ((a (html:text))
-          (b (html:h1)))
-      (is (equal `((:replace (0) ,a ,b)) (com::diff (html:div a) (html:div b))))))
+    (let ((a (text))
+          (b (h1)))
+      (is (equal `((:replace (0) ,a ,b)) (diff (div a) (div b))))))
 
   (it
-    (let ((a (html:text "a"))
-          (b (html:text "b")))
-      (is (equal `((:replace (0) ,a ,b)) (com::diff (html:div a) (html:div b))))))
+    (let ((a (text "a"))
+          (b (text "b")))
+      (is (equal `((:replace (0) ,a ,b)) (diff (div a) (div b))))))
 
   (it
-    (let ((a (html:text "xx"))
-          (b (html:text "xx")))
-      (is (equal nil (com::diff (html:div a) (html:div b))))))
+    (let ((a (text "xx"))
+          (b (text "xx")))
+      (is (equal nil (diff (div a) (div b))))))
 
   (it
-    (is (equal nil (com::diff (html:div "xx") (html:div "xx"))))))
+    (is (equal nil (diff (div "xx") (div "xx"))))))
+
+(test diff/children
+  (it
+    (let ((a (h1))
+          (b (h2))
+          (c (h1))
+          (d (h2)))
+      (is (equal `((:replace (0) ,a ,b)
+                   (:replace (1) ,c ,d))
+                 (diff (div a c) (div b d))))))
+
+  (it
+    (let* ((a (h1))
+           (p-1 (div))
+           (p-2 (div a)))
+      (is (equal `((:insert nil ,p-1 0 ,a))
+                 (diff p-1 p-2)))))
+
+  (it
+    (let* ((a (h1))
+           (p-1 (div))
+           (p-2 (div a a a)))
+      (is (equal `((:insert nil ,p-1 0 ,a)
+                   (:insert nil ,p-1 1 ,a)
+                   (:insert nil ,p-1 2 ,a))
+                 (diff p-1 p-2)))))
+
+  (it
+    (let* ((a (h1))
+           (p-1 (div a))
+           (p-2 (div)))
+      (is (equal `((:remove (0) ,a))
+                 (diff p-1 p-2)))))
+
+  (it
+    (let* ((a (h1))
+           (p-1 (div a a))
+           (p-2 (div)))
+      (is (equal `((:remove (0) ,a)
+                   (:remove (0) ,a))
+                 (diff p-1 p-2))))))
--- test/component/package.lisp
+++ test/component/package.lisp
@@ -4,7 +4,11 @@
   (:nicknames :com-test :wt.component-test :wt.com-test)
   (:use :cl :component :test :alexandria)
   (:shadow :variable)
-  (:export :run!))
+  (:export :run!)
+  (:import-from :html
+                :div
+                :h1 :h2 :h3 :h4
+                :text))
 
 (in-package :component-test)
 (def-suite :component-test)
