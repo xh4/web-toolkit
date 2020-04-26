8ecbadc717db90a32a30a28c2d61393f4a425328
Xiangyu He <xh@coobii.com>
2020-04-26 11:35:16 +0800

    Test against react & react-dom

--- test/javascript/vendor.lisp
+++ test/javascript/vendor.lisp
@@ -4,10 +4,29 @@
 
 (test parse-and-serialize-jquery-3-5-0
   (it
-   (let* ((response (http:get "http://code.jquery.com/jquery-3.5.0.js"))
-          (octets (alexandria::read-stream-content-into-byte-vector
-                   (http:response-body response)))
-          (source (babel:octets-to-string octets)))
-     (unwind-protect
-         (test-parse-and-serialize source)
-       (close (http:response-body response))))))
+    (let* ((response (http:get "http://code.jquery.com/jquery-3.5.0.js"))
+           (octets (http::read-response-body-into-vector response))
+           (source (babel:octets-to-string octets)))
+      (unwind-protect
+          (test-parse-and-serialize source)
+        (close (http:response-body response))))))
+
+(test parse-and-serialize-react-16-13-1
+  (it
+    (let* ((response (http:get "https://unpkg.com/react@16.13.1/umd/react.development.js"))
+           (octets (http::read-response-body-into-vector response))
+           (source (babel:octets-to-string octets)))
+      (unwind-protect
+          (test-parse-and-serialize source)
+        (close (http:response-body response))))))
+
+(test parse-and-serialize-react-dom-16-13-1
+  (it
+    (let* ((response (http:get "https://unpkg.com/react-dom@16.13.1/umd/react-dom.development.js"))
+           (octets (http::read-response-body-into-vector response))
+           (source (babel:octets-to-string octets)))
+      (unwind-protect
+          (test-parse-and-serialize source)
+        (close (http:response-body response))))))
+
+
