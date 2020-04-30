fc5bf9a9a9d6850e8711d3a1e83a96f2b7ce53f1
Xiangyu He <xh@coobii.com>
2020-04-30 21:53:05 +0800

    More es6 tests

--- test/javascript/es6.lisp
+++ test/javascript/es6.lisp
@@ -116,3 +116,198 @@ obj = {
     }
 }
 ")))
+
+(test es6/array-matching
+  (it
+    (test-parse-and-serialize "
+var list = [ 1, 2, 3 ];
+var [ a, , b ] = list;
+[ b, a ] = [ a, b ];
+")))
+
+(test es6/array-matching
+  (it
+    (test-parse-and-serialize "
+var list = [ 1, 2, 3 ];
+var [ a, , b ] = list;
+[ b, a ] = [ a, b ];
+")))
+
+(test es6/object-matching-shorthand-notation
+  (it
+    (test-parse-and-serialize "
+var { op, lhs, rhs } = getASTNode();
+")))
+
+(test es6/object-matching-deep-matching
+  (it
+    (test-parse-and-serialize "
+var { op: a, lhs: { op: b }, rhs: c } = getASTNode();
+")))
+
+(test es6/object-and-array-matching-with-default-value
+  (it
+    (test-parse-and-serialize "
+var obj = { a: 1 };
+var list = [ 1 ];
+var { a, b = 2 } = obj;
+var [ x, y = 2 ] = list;
+")))
+
+(test es6/parameter-context-matching
+  (it
+    (test-parse-and-serialize "
+function f ([ name, val ]) {
+    console.log(name, val);
+}
+function g ({ name: n, val: v }) {
+    console.log(n, v);
+}
+function h ({ name, val }) {
+    console.log(name, val);
+}
+")))
+
+(test es6/fail-soft-destructuring
+  (it
+    (test-parse-and-serialize "
+var [ a = 1, b = 2, c = 3, d ] = list;
+")))
+
+(test es6/export
+  (it
+    (test-parse-and-serialize "
+export function sum (x, y) { return x + y };
+export var pi = 3.141593;
+export * from 'lib/math';
+export { name1, name2, name3 } from 'foo';
+export { default } from 'foo';
+export { foo1 as bar1, foo2 as bar2, foo3 as bar3 } from 'zzz';
+export class ClassName {};
+export var e = 2.71828182846;
+export default (x) => Math.exp(x);
+")))
+
+(test es6/import
+  (it
+    (test-parse-and-serialize "
+import * as math from 'lib/math';
+import { sum, pi } from 'lib/math';
+import exp, { pi, e } from 'lib/mathplusplus';
+")))
+
+(test es6/class-definition
+  (it
+    (test-parse-and-serialize "
+class Shape {
+    constructor (id, x, y) {
+        this.id = id;
+        this.move(x, y);
+    }
+    move (x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+")))
+
+(test es6/class-inheritance
+  (it
+    (test-parse-and-serialize "
+class Rectangle extends Shape {
+    constructor (id, x, y, width, height) {
+        super(id, x, y);
+        this.width  = width;
+        this.height = height;
+    }
+}
+class Circle extends Shape {
+    constructor (id, x, y, radius) {
+        super(id, x, y);
+        this.radius = radius;
+    }
+}
+")))
+
+(test es6/static-member
+  (it
+    (test-parse-and-serialize "
+class Rectangle extends Shape {
+    static defaultRectangle () {
+        return new Rectangle('default', 0, 0, 100, 100);
+    }
+}
+class Circle extends Shape {
+    static defaultCircle () {
+        return new Circle('default', 0, 0, 100);
+    }
+}
+")))
+
+(test es6/getter-and-setter
+  (it
+    (test-parse-and-serialize "
+class Rectangle {
+    constructor (width, height) {
+        this._width  = width;
+        this._height = height;
+    }
+    set width  (width)  { this._width = width;               }
+    get width  ()       { return this._width;                }
+    set height (height) { this._height = height;             }
+    get height ()       { return this._height;               }
+    get area   ()       { return this._width * this._height; }
+}
+")))
+
+(test es6/iterator
+  (it
+    (test-parse-and-serialize "
+let fibonacci = {
+    [Symbol.iterator]() {
+        let pre = 0, cur = 1
+        return {
+           next () {
+               [ pre, cur ] = [ cur, pre + cur ]
+               return { done: false, value: cur }
+           }
+        }
+    }
+}
+")))
+
+(test es6/for-of
+  (it
+    (test-parse-and-serialize "
+for (let n of fibonacci) {
+    if (n > 1000)
+        break
+    console.log(n)
+}
+")))
+
+(test es6/generator-function
+  (it
+    (test-parse-and-serialize "
+function* range (start, end, step) {
+    while (start < end) {
+        yield start;
+        start += step;
+    }
+}
+")))
+
+(test es6/generator-method
+  (it
+    (test-parse-and-serialize "
+class Clz {
+    * bar () {
+
+    }
+}
+let Obj = {
+    * foo () {
+
+    }
+}
+")))
