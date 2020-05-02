ae6d3ef9f95b5c2afe0299dc0bb3bff2e0cd293f
Xiangyu He <xh@coobii.com>
2020-05-02 21:09:25 +0800

    Add tokenizer test

--- test/html/tokenize.lisp
+++ test/html/tokenize.lisp
@@ -1,3 +1,34 @@
 (in-package :html-test)
 
 (in-suite :html-test)
+
+(test tokenize/doctype
+  (it
+    (let ((token (first (html::tokenize "<!doctype html>"))))
+      (is (eq 'html::doctype (type-of token)))
+      (is (equal "html" (slot-value token 'html::name)))))
+
+  (it
+    (let ((token (first (html::tokenize "<!DOCTYPE html>"))))
+      (is (eq 'html::doctype (type-of token)))
+      (is (equal "html" (slot-value token 'html::name))))))
+
+(test tokenize/named-character-reference
+  (it
+    (let ((token (first (html::tokenize "&lt;"))))
+      (is (eq #\< token))))
+
+  (it
+    (signals error (html::tokenize "I'm &notit; I tell you")))
+
+  (it
+    (handler-bind ((html::parse-error (lambda (c) (continue))))
+      (let ((chars (html::tokenize "I'm &notit; I tell you")))
+        (is (equal "I'm ¬it; I tell you" (coerce chars 'string))))))
+
+  (it
+    (finishes (html::tokenize "I'm &notin; I tell you")))
+
+  (it
+    (let ((chars (html::tokenize "I'm &notin; I tell you")))
+      (is (equal "I'm ∉ I tell you" (coerce chars 'string))))))
