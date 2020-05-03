2bab0b86ceea910c6f23b7474182c3a6610c9abf
Xiangyu He <xh@coobii.com>
2020-05-04 00:45:02 +0800

    Print text data

--- html/text.lisp
+++ html/text.lisp
@@ -3,7 +3,9 @@
 (defclass text (dom:text) ())
 
 (defmethod print-object ((text text) stream)
-  (print-unreadable-object (text stream :type t :identity t)))
+  (print-unreadable-object (text stream :type t :identity t)
+    ;; TODO: trim text data
+    (format stream "~S" (slot-value text 'dom:data))))
 
 (defun text (&optional data)
   (check-type data (or null string))
