11c890730a90a27a0137c0666a2ba4ee6fafdc2a
Xiangyu He <xh@coobii.com>
2020-05-27 17:43:14 +0800

    Export SVG symbols

--- svg/package.lisp
+++ svg/package.lisp
@@ -3,4 +3,43 @@
 (defpackage :svg
   (:nicknames :wt.svg)
   (:use :cl :alexandria)
-  (:shadow :use :symbol))
+  (:shadow :use :symbol)
+  (:export
+   :element
+   :graphics-element
+   :geometry-element
+   :container-element
+   :structural-element
+   :shape-element
+   :renderable-element
+   :never-rendered-element
+   :descriptive-element
+   :graphics-referencing-element
+   :structurally-external-element
+   :text-content-element
+   :text-content-child-element
+   :animation-element
+   :paint-server-element
+   :svg
+   :g
+   :defs
+   :desc
+   :metadata
+   :title
+   :symbol
+   :use
+   :switch
+   :path
+   :rect
+   :circle
+   :ellipse
+   :line
+   :polyline
+   :polygon
+   :text
+   :tspan
+   :text-path
+   :image
+   :marker
+   :a
+   :view))
